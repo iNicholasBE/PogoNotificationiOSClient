2f5739b640f8caf1391af54f292ead9dffc091c5
Nicholas <nic.dhondt@gmail.com>
2016-07-22 16:27:35 +0200

    Initial Commit

---
+++ PokeInfoTests/PokeInfoTests.swift
@@ -0,0 +1,36 @@
+//
+//  PokeInfoTests.swift
+//  PokeInfoTests
+//
+//  Created by Nicholas D'hondt on 22/07/16.
+//  Copyright © 2016 Nicholas D'hondt. All rights reserved.
+//
+
+import XCTest
+@testable import PokeInfo
+
+class PokeInfoTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
