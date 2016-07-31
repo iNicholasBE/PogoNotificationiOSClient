6225164fdcf112d5f15a77a281ecaa9ba95e0666
Nicholas <nic.dhondt@gmail.com>
2016-07-31 03:58:42 +0200

    Clean-up code, background music loop, switch fix

--- PokeInfo/ViewController.swift
+++ PokeInfo/ViewController.swift
@@ -28,12 +28,10 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
     var oldLocation = CLLocation();
     
     var allPokemons = [Pokemon]();
-    var player: AVQueuePlayer!
+    var audioPlayer = AVAudioPlayer()
     var initizalized = false;
     var pokemonsInArea = [Double : Pokemon]();
     
-    
-
     
     lazy var locationManager: CLLocationManager! = {
         let manager = CLLocationManager()
@@ -44,69 +42,53 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         return manager
     }()
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         locationManager.startUpdatingLocation()
         pokeMap.delegate = self
-
+        startAudio()
         var helloWorldTimer = NSTimer.scheduledTimerWithTimeInterval(5.0, target: self, selector: #selector(ViewController.fireCalls), userInfo: nil, repeats: true)
-
-        
-        var error: NSError?
-        
-        var success: Bool
-        do {
-            try AVAudioSession.sharedInstance().setCategory(
-                AVAudioSessionCategoryPlayAndRecord,
-                withOptions: .DefaultToSpeaker)
-            success = true
-        } catch var error1 as NSError {
-            error = error1
-            success = false
-        }
-        if !success {
-            NSLog("Failed to set audio session category.  Error: \(error)")
-        }
-        let songNames = ["higher"]
-        let songs = songNames.map { AVPlayerItem(URL:
-            NSBundle.mainBundle().URLForResource($0, withExtension: "mp3")!) }
-        
-        player = AVQueuePlayer(items: songs)
-        player.actionAtItemEnd = .Advance
-        player.play();
-        player.volume = 0.00001;
-        
-
-        // Do any additional setup after loading the view, typically from a nib.
     }
+    
+    override func viewDidAppear(animated: Bool) {
+        //
+        self.pokeMap.showsUserLocation = true
+    }
+    
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-    @IBAction func accuracyChanged(sender: UISegmentedControl) {
-        let accuracyValues = [
-            kCLLocationAccuracyBestForNavigation,
-            kCLLocationAccuracyBest,
-            kCLLocationAccuracyNearestTenMeters,
-            kCLLocationAccuracyHundredMeters,
-            kCLLocationAccuracyKilometer,
-            kCLLocationAccuracyThreeKilometers]
-        
-        locationManager.desiredAccuracy = accuracyValues[sender.selectedSegmentIndex];
-    }
-    
-    @IBAction func changed(sender: UISwitch) {
-        if (sender.on) {
-            //locationManager.startUpdatingLocation()
-            player.play()
-        } else {
-            //locationManager.stopUpdatingLocation()
-            player.pause()
+    func startAudio() {
+        let filePath = NSBundle.mainBundle().pathForResource("sea", ofType: "mp3")
+        let fileURL = NSURL.fileURLWithPath(filePath!)
+        do {
+            audioPlayer = try AVAudioPlayer.init(contentsOfURL: fileURL, fileTypeHint: AVFileTypeMPEGLayer3)
+            audioPlayer.numberOfLoops = -1
+            audioPlayer.volume = 1
+        } catch {
+            self.presentViewController(UIAlertController.init(title: "Error", message: "Error Message", preferredStyle: .Alert), animated: true, completion: nil)
         }
+        audioPlayer.play()
     }
+
     
+    @IBAction func switchChanged(sender: UISwitch) {
+        
+        if sender.on{
+            audioPlayer.play()
+            timer.fire()
+            locationManager.startUpdatingLocation()
+        }
+        else{
+            audioPlayer.pause()
+            timer.invalidate()
+            locationManager.stopUpdatingLocation()
+        }
+        
+    }
     
     
     func sendNotification(description:String){
@@ -135,17 +117,13 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
             annotation.coordinate = locatie;
             annotation.title = pokemon.pokemon_id.description;
             print (annotation.title)
-          
-            
+
             self.pokeMap.addAnnotation(annotation)
             //self.pokeMap.showAnnotations(self.pokeMap.annotations, animated: true)
             self.pokeMap.zoomEnabled = true;
             self.showAnnotations();
             //self.pokeMap.selectAnnotation(self.pokeMap.annotations[0], animated: true)
             //self.pokeMap.selectAnnotation(self.pokeMap.annotations[self.pokeMap.annotations.count], animated: true)
-        
-        
-        
     }
     
     func mapView(mapView: MKMapView!,
@@ -179,13 +157,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         return pokemon
     }
     
-    
-    
-    // Function to check if the pokemon is already added to the array or not
-    func addToPokemonArray(pokemon:Pokemon){
-        
-    }
-    
+
     func fireCalls(){
         
         let newLocation = currentLocation;
@@ -201,9 +173,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
                     var tempWild = [String]();
                     for var pokemon in wild{
                         if !self.wildArr.contains(pokemon["pokemon_name"].string!){
-
                             if UIApplication.sharedApplication().applicationState == .Active {
-                        
                             } else {
                                 self.sendNotification(pokemon["pokemon_name"].string! + " is wild");
                             }
@@ -215,12 +185,8 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
                     }
                     // Overwrite global wildArr with new updated one
                     self.wildArr = tempWild;
-                    
-                    
                 }
                 self.showPokemonsAndDeleteOldOnes()
-
-                
         };
         
         
@@ -266,7 +232,6 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
                     }
                 }
                 print (json);
-                
         };
     }
     
@@ -287,10 +252,7 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
     }
     
     func showAnnotations(){
-        for var annotation in self.pokeMap.annotations{
-            //pokeMap.selectAnnotation(annotation, animated: true)
-        }
-    }
+            }
     
     func deleteAnnotations(){
         for var annotation in self.pokeMap.annotations{
@@ -298,49 +260,35 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         }
     }
     
-    /*
-    func locationManager(manager: CLLocationManager, didFinishDeferredUpdatesWithError error: NSError!) {
-        // Stop deferring updates
-        self.deferringUpdates = false
-        
-        // Adjust for the next goal
-    }
- */
+    func setMapToUserLocation(newLocation: CLLocation){
+        let latDelta:CLLocationDegrees = 0.005
+        let lonDelta:CLLocationDegrees = 0.005
+        let span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
+        let location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(newLocation.coordinate.latitude, newLocation.coordinate.longitude)
+        let region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
+        self.pokeMap.setRegion(region, animated: true)
+        self.pokeMap.showsUserLocation = true
 
+    }
+    
     
     func locationManager(manager: CLLocationManager, didUpdateToLocation newLocation: CLLocation, fromLocation oldLocation: CLLocation) {
         
-        
-            var interval  = NSTimeInterval();
+        var interval  = NSTimeInterval();
             interval = 30;
-            
         
-        
-            // Check  every 10 meters
-            var distance = CLLocationDistance();
-            distance = 10;
-            locationManager.distanceFilter = distance;
-        
-        
-        
+        // Check  every 10 meters
+        var distance = CLLocationDistance();
+        distance = 10;
+        locationManager.distanceFilter = distance;
+    
      
         // Zoom to user location
         self.pokeMap.showsUserLocation = true
         
         if (!initizalized){
             initizalized = true;
-        
-        var latDelta:CLLocationDegrees = 0.005
-        
-        var lonDelta:CLLocationDegrees = 0.005
-        
-        var span:MKCoordinateSpan = MKCoordinateSpanMake(latDelta, lonDelta)
-        
-        var location:CLLocationCoordinate2D = CLLocationCoordinate2DMake(newLocation.coordinate.latitude, newLocation.coordinate.longitude)
-        
-        var region:MKCoordinateRegion = MKCoordinateRegionMake(location, span)
-        
-        self.pokeMap.setRegion(region, animated: true)
+            setMapToUserLocation(newLocation)
         }
         
         do {
@@ -351,69 +299,10 @@ class ViewController: UIViewController,CLLocationManagerDelegate,MKMapViewDelega
         }
 
         
-        if UIApplication.sharedApplication().applicationState == .Active {
-            
-        } else {
-            NSLog("App is backgrounded. New location is %@", newLocation)
+        if !(UIApplication.sharedApplication().applicationState == .Active) {
+             NSLog("App is backgrounded. New location is %@", newLocation)
         }
     }
 
 
 }
-extension NSDate {
-    func isGreaterThanDate(dateToCompare: NSDate) -> Bool {
-        //Declare Variables
-        var isGreater = false
-        
-        //Compare Values
-        if self.compare(dateToCompare) == NSComparisonResult.OrderedDescending {
-            isGreater = true
-        }
-        
-        //Return Result
-        return isGreater
-    }
-    
-    func isLessThanDate(dateToCompare: NSDate) -> Bool {
-        //Declare Variables
-        var isLess = false
-        
-        //Compare Values
-        if self.compare(dateToCompare) == NSComparisonResult.OrderedAscending {
-            isLess = true
-        }
-        
-        //Return Result
-        return isLess
-    }
-    
-    func equalToDate(dateToCompare: NSDate) -> Bool {
-        //Declare Variables
-        var isEqualTo = false
-        
-        //Compare Values
-        if self.compare(dateToCompare) == NSComparisonResult.OrderedSame {
-            isEqualTo = true
-        }
-        
-        //Return Result
-        return isEqualTo
-    }
-    
-    func addDays(daysToAdd: Int) -> NSDate {
-        let secondsInDays: NSTimeInterval = Double(daysToAdd) * 60 * 60 * 24
-        let dateWithDaysAdded: NSDate = self.dateByAddingTimeInterval(secondsInDays)
-        
-        //Return Result
-        return dateWithDaysAdded
-    }
-    
-    func addHours(hoursToAdd: Int) -> NSDate {
-        let secondsInHours: NSTimeInterval = Double(hoursToAdd) * 60 * 60
-        let dateWithHoursAdded: NSDate = self.dateByAddingTimeInterval(secondsInHours)
-        
-        //Return Result
-        return dateWithHoursAdded
-    }
-}
-
